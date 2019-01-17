75bf7d1a665b9fd258ee72b4ac1e8d49c9dd7f80
ipandz00 <ipandz00@gmail.com>
2019-01-17 13:53:05 +0100

    Added task controller

---
+++ app/controllers/task.controller.js
@@ -0,0 +1,26 @@
+const Task = require('../models/task.model.js');
+
+// Create and Save a new Task
+exports.create = (req, res) => {
+
+};
+
+// Retrieve and return all tasks from the database.
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single task with a taskId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a task identified by the taskId in the request
+exports.update = (req, res) => {
+
+};
+
+// Delete a task with the specified taskId in the request
+exports.delete = (req, res) => {
+
+};
