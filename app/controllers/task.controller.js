077bdbca921f9066288ddba86311c137d9976077
ipandz00 <ipandz00@gmail.com>
2019-01-17 14:09:58 +0100

    Task controller - create new task added

--- app/controllers/task.controller.js
+++ app/controllers/task.controller.js
@@ -2,7 +2,28 @@ const Task = require('../models/task.model.js');
 
 // Create and Save a new Task
 exports.create = (req, res) => {
+	// Validate request
+    if(!req.body.title) {
+        return res.status(400).send({
+            message: "Task title can not be empty."
+        });
+    }
 
+    // Create a Task
+    const task = new Task({
+        title: req.body.title, 
+        description: req.body.description || 'No description availible.'
+    });
+
+    // Save Task in the database
+    task.save()
+    .then(data => {
+        res.send(data);
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occurred while creating the Task."
+        });
+    });
 };
 
 // Retrieve and return all tasks from the database.
