de3fd86766da84040b04a406382bc28c22f75742
ipandz00 <ipandz00@gmail.com>
2019-01-18 13:58:38 +0100

    Delete modal + backwards button added to single task overview

--- frontend-app/src/pages/Task.js
+++ frontend-app/src/pages/Task.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
-import { getTask } from '../api.js';
-import { Link } from "react-router-dom";
+import { getTask, deleteTasks, updateTask } from '../api.js';
+import { Link, Redirect } from "react-router-dom";
 import Dialog from '../containers/AddUpdateDialogContainer.js';
 import TaskDetails from '../containers/TaskDetailsContainer.js';
 import Button from '@material-ui/core/Button';
+import Fab from '@material-ui/core/Fab';
+import Icon from '@material-ui/core/Icon';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import BackIcon from '@material-ui/icons/Reply';
+import DeleteDialog from '../containers/DeleteDialogContainer.js';
 
 export default class Task extends Component {
 	constructor(props) {
@@ -11,11 +17,14 @@ export default class Task extends Component {
 
 		this.state = {
 			taskData: null,
-			updateModal: false
+			updateModal: false,
+			deleteModal: false,
+			redirect: false
 		}
 
 		this.handleClose = this.handleClose.bind(this);
 		this.handleSave = this.handleSave.bind(this);
+		this.handleDeleteDialog = this.handleDeleteDialog.bind(this);
 	}
 
 	componentDidMount() {
@@ -30,14 +39,33 @@ export default class Task extends Component {
 	}
 
 	handleSave(data) {
-		console.log(data);
+		updateTask(this.state.taskData._id, data.title, data.description)
+			.then((response) => {
+				this.setState({taskData: response});
+			});
 		this.setState({updateModal: false});
 	}
 
+	handleDeleteDialog(ans) {
+		if(ans) {
+			this.handleTaskDelete();
+		}
+		this.setState({deleteModal: false});
+	}
+
+	handleTaskDelete(id) {
+		deleteTasks([this.state.taskData._id])
+		.then((response) => {
+			this.setState({tasksData: null, redirect: true});
+		})
+	}
+
 	render() {
+		if (this.state.redirect) {
+	       return <Redirect to='/tasks'/>;
+	     }
 		return (
 			<React.Fragment>
-				<Link to='/tasks'>Go back</Link>
 				{this.state.taskData && 
 					<React.Fragment>
 						<TaskDetails data={this.state.taskData}/>
@@ -47,7 +75,21 @@ export default class Task extends Component {
 							handleClose={this.handleClose}
 							handleSave={this.handleSave}
 						/>
-						<Button onClick={()=>this.setState({updateModal: true})}>Edit</Button>
+						<DeleteDialog
+							open={this.state.deleteModal}
+							getAnswer={this.handleDeleteDialog} 
+						/>
+						<Link to='/tasks'>
+							<Fab aria-label="back" >
+						    	<BackIcon />
+						    </Fab>
+						</Link>
+						<Fab color="primary" aria-label="Edit" onClick={()=>this.setState({updateModal: true})}>
+					    	<EditIcon />
+					    </Fab>
+					    <Fab color="secondary" aria-label="Delete" onClick={()=>this.setState({deleteModal: true})}>
+					    	<DeleteIcon />
+					    </Fab>
 					</React.Fragment>
 				}
 			</React.Fragment>
